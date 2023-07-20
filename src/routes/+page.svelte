e6db969cfc3c8bead3a50af0bc738b697be403dc
Tucker Gordon <gordon_tucker@bah.com>
2023-07-20 17:56:51 -0400

    Updated intro page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<h1>Hi there</h1>
-<p>
-  My name is Tucker and this is my website. Things are still under construction, but I hope to have
-  the site more built out soon!
-</p>
+<div class="prose dark:prose-invert p-8 mx-auto">
+  <h1>Hi there!</h1>
+  <p>
+    My name is Tucker and this is my website. Things are still under construction... but stay tuned for more updates!
+  </p>
+</div>
