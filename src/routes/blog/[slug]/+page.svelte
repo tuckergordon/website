6991c0452ac64398c36a9785c92897c076bfcb49
Tucker Gordon <tucker.gordon18@gmail.com>
2023-07-25 20:20:25 -0400

    Fixed blog route
    Changed `Blog/` to `blog` and had to set git to take case into account:
    
    ```shell
    git config core.ignorecase false
    ```

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { formatDate } from '$lib/utils';
+
+  export let data;
+</script>
+
+<!-- SEO -->
+<svelte:head>
+  <title>{data.meta.title}</title>
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article class="prose dark:prose-invert mx-auto p-4 pt-12">
+  <hgroup>
+    <h1>{data.meta.title}</h1>
+    <p class="italic">{formatDate(data.meta.date)}</p>
+  </hgroup>
+
+  <div class="content">
+    <svelte:component this={data.content} />
+  </div>
+</article>
