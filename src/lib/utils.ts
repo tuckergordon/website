1e80ac3fa9be2485036219dbed9790fad7235af8
Tucker Gordon <tucker.gordon18@gmail.com>
2025-02-20 22:18:12 -0500

    Post: portland, ME recs
    New blog post with eat/drink recs for portland, ME.
    
    Also realized `formatDate` was giving the wrong dates because of UTC
    issues.
    
    And added numbers in front of post names to make them easier to sort in
    the file system.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 type DateStyle = Intl.DateTimeFormatOptions['dateStyle'];
 
-export function formatDate(date: Date, dateStyle: DateStyle = 'medium', locales = 'en') {
-  const formatter = new Intl.DateTimeFormat(locales, { dateStyle });
-  return formatter.format(new Date(date));
+export function formatDate(
+  date: Date | string,
+  dateStyle: DateStyle = 'medium',
+  locales = 'en',
+  timeZone = 'America/New_York',
+) {
+  if (typeof date === 'string') {
+    const [year, month, day] = date.split('-').map(Number);
+    date = new Date(year, month - 1, day, 0, 0, 0);
+  }
+
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    throw new Error('Invalid date');
+  }
+
+  const formatter = new Intl.DateTimeFormat(locales, { dateStyle, timeZone });
+  return formatter.format(date);
 }
