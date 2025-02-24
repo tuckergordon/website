d62976af2ffdaefd219eee948bbbddf3ee622bf9
Tucker Gordon <tucker.gordon18@gmail.com>
2025-02-23 21:30:29 -0500

    Add Vercel speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,10 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
-// Vercel analytics
+// Vercel tools
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights();
 
 export async function load({ url }) {
   return {
