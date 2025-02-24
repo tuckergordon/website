dd4343259a84ec2c0f0c123a30c133b5370241bd
Tucker Gordon <tucker.gordon18@gmail.com>
2025-02-23 20:58:25 -0500

    Add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,9 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+// Vercel analytics
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 export async function load({ url }) {
   return {
     url: url.pathname,
